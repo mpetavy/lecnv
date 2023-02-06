54b35b8c98684a59bd1fd4d3853459a1cc085154
mpetavy <mpetavy>
2023-02-06 19:24:01 +0100

    common@v1.5.26 update

--- main.go
+++ main.go
@@ -21,7 +21,7 @@ var (
 )
 
 func init() {
-	common.Init(false, "1.0.0", "", "", "2019", "Line ending converter", "mpetavy", fmt.Sprintf("https://github.com/mpetavy/%s", common.Title()), common.APACHE, nil, nil, nil, run, 0)
+	common.Init("1.0.0", "", "", "2019", "Line ending converter", "mpetavy", fmt.Sprintf("https://github.com/mpetavy/%s", common.Title()), common.APACHE, nil, nil, nil, run, 0)
 
 	filemask = flag.String("f", "", "input file or STDIN")
 	recursive = flag.Bool("r", false, "recursive directory search")
